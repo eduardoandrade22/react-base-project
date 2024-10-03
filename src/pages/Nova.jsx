9ba0baf0675ecc676ddf28791f0171647376ed2f
EDUARDO DE ARAUJO ROLIM CERSOSIMO DE ANDRADE <202307539767@alunos.ibmec.edu.br>
2024-10-03 17:22:48 -0300

    atualizado

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,9 @@
+import NovoComponente from "../components/NovoComponente";
+const Nova = () => {
+    return(<>
+    <h1>TITULO</h1>
+    <p>ooiii</p>
+    <NovoComponente />
+    </>);
+}
+export default Nova;
